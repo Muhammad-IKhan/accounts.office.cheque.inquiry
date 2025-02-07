cbb19fc0a4392d915d0744d76425c22a974c0027
Muhammad Ismail Khan <muhammadismailk9@gmail.com>
2025-02-07 07:54:54 +0500

    used local storage or IndexedDB in script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//registration
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js')
     .then((registration) => {
@@ -6,4 +8,21 @@ if ('serviceWorker' in navigator) {
     .catch((error) => {
       console.error('Service Worker registration failed:', error);
     });
+}
+// fetching and keeping the XML data data for use local storage instead IndexedDB
+fetch('https://muhammad-ikhan.github.io/accounts.office.cheque.inquiry/data.xml')
+  .then((response) => response.text())
+  .then((data) => {
+    // Save XML data to local storage
+    localStorage.setItem('xmlData', data);
+    console.log('XML data saved to local storage');
+  })
+  .catch((error) => {
+    console.error('Error fetching XML data:', error);
+  });
+
+// Retrieve XML data from local storage
+const xmlData = localStorage.getItem('xmlData');
+if (xmlData) {
+  console.log('XML data loaded from local storage:', xmlData);
 }
