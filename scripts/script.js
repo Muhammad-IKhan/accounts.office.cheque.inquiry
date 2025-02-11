6432bfded5f3c68b8967af8b56f9685176db2aad
Muhammad Ismail Khan <muhammadismailk9@gmail.com>
2025-02-11 12:34:01 +0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -75,7 +75,8 @@ function parseXMLToTable(xmlString = null) {
     try {
 
         // Replace newline character (&#10;) with <br /> tag
-        let xmlData.replace(/&#10;/g, '<br />');
+        let xmlData;
+        xmlData.replace(/&#10;/g, '<br />');
 
         const parser = new DOMParser();
         const xmlDoc = parser.parseFromString(xmlString || xmlData, "text/xml");
