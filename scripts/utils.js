6906220d475de07649bab2808615d7c11ccb473e
Muhammad Ismail Khan <muhammadismailk9@gmail.com>
2025-02-11 21:52:47 +0500

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,4 +1,4 @@
-// utils.js for error handaling / utility functions
+// utils.js for utility functions
 function handleError(error) {
     console.error('Showing error to user:', error);
     showError(error);
