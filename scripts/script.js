74f93bf0dbd9dd6aca044c2294b55798080411e0
Muhammad Ismail Khan <muhammadismailk9@gmail.com>
2025-02-10 19:57:52 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -241,7 +241,7 @@ function searchAndFilterXML() {
         }
         
         // Handle empty or NaN values specifically for the "Amount" cell
-        if (cell.classList.contains('amount') && (isNaN(cell.textContent.trim()) || cell.textContent.trim() === '')) {
+        if (cell.classList.contains('amount') && (isNaN(cell.textContent.trim()) || cell.textContent.trim() === '-')) {
             cell.textContent = '-';
             console.log(`Empty or NaN amount cell detected, replaced with "-"`);
         }
