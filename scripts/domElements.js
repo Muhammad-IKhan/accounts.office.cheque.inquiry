d7f68b1df5638dfdc3b85005e5fdb44f808ead05
Muhammad Ismail Khan <muhammadismailk9@gmail.com>
2025-02-11 19:32:33 +0500

    Update domElements.js

--- scripts/domElements.js
+++ scripts/domElements.js
@@ -1,1 +1,18 @@
+// domElements.js
 
+// Global variables to store DOM elements
+export let tableBody, searchInput, tableContainer, emptyState, resultContainer;
+export let xmlData;
+
+/**
+ * Initializes DOM elements
+ */
+export function initializeDOMElements() {
+    console.log('Initializing DOM elements...');
+    
+    tableBody = document.getElementById('checksTable');
+    searchInput = document.getElementById('search');
+    tableContainer = document.getElementById('tableContainer');
+    emptyState = document.getElementById('emptyState');
+    resultContainer = document.getElementById('result');
+}
