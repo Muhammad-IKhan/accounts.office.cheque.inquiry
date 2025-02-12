b581a849faffe557e1ca5077fd3e42ffc0639691
Muhammad Ismail Khan <muhammadismailk9@gmail.com>
2025-02-12 19:30:57 +0500

    Create store.js

---
+++ scripts/store.js
@@ -0,0 +1,13 @@
+// store.js
+export const store = {
+    xmlData: null,
+    sortState: {
+        currentColumn: null,
+        direction: 'asc'
+    },
+    paginationState: {
+        currentPage: 1,
+        rowsPerPage: PAGINATION_CONFIG.DEFAULT_PAGE_SIZE,
+        totalPages: 1
+    }
+};
